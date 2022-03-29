f41d79fd450326369f56334f5d44cf818e9aa0eb
oscaru21 <oscarumana1021@gmail.com>
2022-03-29 09:23:39 -0600

    first commit

---
+++ src/main/java/com/oumana/TwitterAccessLayerApplication.java
@@ -0,0 +1,13 @@
+package com.oumana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAccessLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAccessLayerApplication.class, args);
+	}
+
+}
